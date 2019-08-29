452cc3b9fd4bb09c85ff570dbbb5211454b2b099
Zach Smialek <zsmialek@freewave.com>
2019-08-28 20:32:33 -0600

    Init commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	//import the Paho Go MQTT library
+	"os"
+	"time"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+//define a function for the default message handler
+var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
+}
+
+func main() {
+	//create a ClientOptions struct setting the broker address, clientid, turn
+	//off trace output and set the default message handler
+	opts := MQTT.NewClientOptions().AddBroker("tcp://iot.eclipse.org:1883")
+	opts.SetClientID("go-simple")
+	opts.SetDefaultPublishHandler(f)
+
+	//create and start a client using the above ClientOptions
+	c := MQTT.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	//subscribe to the topic /go-mqtt/sample and request messages to be delivered
+	//at a maximum qos of zero, wait for the receipt to confirm the subscription
+	if token := c.Subscribe("go-mqtt/sample", 0, nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	time.Sleep(3 * time.Second)
+
+	//unsubscribe from /go-mqtt/sample
+	if token := c.Unsubscribe("go-mqtt/sample"); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	c.Disconnect(250)
+}
